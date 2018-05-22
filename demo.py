b3db8c850c51558471d37a0b5ac6ea35a2a92280
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-05-22 15:19:05 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,11 +8,10 @@ points = genfromtxt("data.csv", delimiter=",")
 intercept, slope = 0, 0
 
 # printing initial output values one by one
-print("\nInitially..,")
+print("\nInitial values :")
 print("intercept =", intercept)
 print("slope =", slope)
-print("error =", mean(
-    (points[:, 1] - (slope * points[:, 0] + intercept)) ** 2))
+print("error =", mean((points[:, 1] - (slope * points[:, 0] + intercept)) ** 2))
 
 # itteration count to 10 times the length of data set
 num_iterations = len(points) * 10
@@ -23,8 +22,7 @@ learning_rate = 1 / (num_iterations * 10)
 # running the loop for 10x the length of data to generate accurate slope and intercept
 for i in range(num_iterations):
     # calculating average change in values when compared with predected and actual values
-    avg_change = mean(2 / len(points) *
-                      (points[:, 1] - (slope * points[:, 0]) + intercept))
+    avg_change = mean(2 / len(points) * (points[:, 1] - (slope * points[:, 0]) + intercept))
 
     # using that average change to adjust intercept
     intercept += avg_change * learning_rate
@@ -33,8 +31,7 @@ for i in range(num_iterations):
     slope += (avg_change * sum(points[:, 0])) * learning_rate
 
 # printing final output values one by one
-print("\nAfter", num_iterations, "itterations..,")
+print("\nValues After", num_iterations, " itterations :")
 print("intercept =", intercept)
 print("slope =", slope)
-print("error =", mean(
-    (points[:, 1] - (slope * points[:, 0] + intercept)) ** 2))
+print("error =", mean((points[:, 1] - (slope * points[:, 0] + intercept)) ** 2))
